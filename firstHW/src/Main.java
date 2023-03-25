d2ac76dedd9729105cef0cc8ad85e08531834098
StrokachD <peredplata16@gmail.com>
2023-03-25 17:03:37 +0200

    my Random number

--- firstHW/src/Main.java
+++ firstHW/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String logIn = "Нуу, поехали!";
+        System.out.println(logIn);
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Как тебя зовут? ");
+        String name = myScanner.nextLine();
+
+        int a = (int) (Math.random() * (100 + 0)) + 0;
+
+        System.out.println("Попробуй-ка угадать число от 0 до 100");
+        Scanner myScanner1 = new Scanner(System.in);
+        int b = 0;
+        while (b != a) {
+
+            b = myScanner1.nextInt();
+            if (b < a) {
+                System.out.println("Надо больше. Повтори попытку.");
+            }
+            if (b > a) {
+                System.out.println("Надо меньше. Повтори попытку.");
+            }
+
+        } System.out.println("Да! Поздравляю," + name + "!") ;
     }
 }
